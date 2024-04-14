f58500bd1b412cf01dda524cebec7814de69a593
doinel1a <doinel1atanasiu@gmail.com>
2024-04-14 18:50:54 +0200

    feat: install & configure expo vector icons

--- lib/hooks/use-load-fonts.ts
+++ lib/hooks/use-load-fonts.ts
@@ -3,12 +3,15 @@
 
 import { useFonts } from 'expo-font';
 
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+
 export default function useLoadFonts() {
   const [areFontsLoaded, fontsError] = useFonts({
     'Roboto-Thin': require('../../assets/fonts/Roboto-Thin.ttf'),
     'Roboto-Regular': require('../../assets/fonts/Roboto-Regular.ttf'),
     'Roboto-Medium': require('../../assets/fonts/Roboto-Medium.ttf'),
-    'Roboto-Bold': require('../../assets/fonts/Roboto-Bold.ttf')
+    'Roboto-Bold': require('../../assets/fonts/Roboto-Bold.ttf'),
+    ...FontAwesome.font
   });
 
   return {
