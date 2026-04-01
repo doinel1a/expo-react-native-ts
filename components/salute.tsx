9e713058c00c7814f6141f25aef389b72671aa1a
doinel1a <doinel1atanasiu@gmail.com>
2026-04-01 18:23:08 +0200

    chore: upgrade nativewind to v5

--- components/salute.tsx
+++ components/salute.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-import { Text } from 'react-native';
+import { Text } from './tw';
 
-type TSalute = {
+type TSalute = Readonly<{
   text: string;
-};
+}>;
 
-export default function Salute({ text }: Readonly<TSalute>) {
+export default function Salute({ text }: TSalute) {
   return <Text className='text-xl font-semibold'>{text}</Text>;
 }
